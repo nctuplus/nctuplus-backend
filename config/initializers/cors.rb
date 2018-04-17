cffaaefba88ac9da6b18a31bd31d96f57733f139
deror1869107 <sungyuanyao@gmail.com>
2018-04-17 21:12:08 +0800

    allow cors when development

---
+++ config/initializers/cors.rb
@@ -0,0 +1,13 @@
+# config/initializers/cors.rb
+
+if Rails.env.development?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+  end
+end
