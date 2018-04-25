a18af22b48986da73500c4462723b9d9d30b196b
yysung <sungyuanyao@gmail.com>
2018-04-25 13:34:56 +0800

    use fast_jsonapi gem

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::UserController < ApiController
   before_action :check_login, :only => [:index]
 
   def index
-    render json: { data: current_v1_user.as_json(except: [ :provider, :tokens ], methods: :avatar_url) }
+    render json: UserSerializer.new(current_v1_user).serialized_json
   end
 end
